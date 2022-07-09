7ab35004aeb53a5f1060ef6d0b623236e5a5731b
Carl Stephens <carlst99@outlook.co.nz>
2022-07-09 23:44:51 +1200

    Integrate System.IO.Abstractions into the CachingManifestService.cs.

--- Sanctuary.Census.ClientData/Services/CachingManifestService.cs
+++ Sanctuary.Census.ClientData/Services/CachingManifestService.cs
@@ -1,5 +1,6 @@
 ﻿using Sanctuary.Census.ClientData.Objects;
 using System.IO;
+using System.IO.Abstractions;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,9 +12,14 @@ namespace Sanctuary.Census.ClientData.Services;
 public class CachingManifestService : ManifestService
 {
     /// <summary>
+    /// Gets the file system implementation.
+    /// </summary>
+    protected IFileSystem FileSystem { get; }
+
+    /// <summary>
     /// Gets the directory under which files are cached.
     /// </summary>
-    protected readonly string CacheDirectory;
+    public string CacheDirectory { get; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="CachingManifestService"/> class.
@@ -21,25 +27,37 @@ public class CachingManifestService : ManifestService
     /// <param name="httpClient">The HTTP client to use.</param>
     /// <param name="appDataDirectory">The path to the app data directory to use.</param>
     public CachingManifestService(HttpClient httpClient, string appDataDirectory)
+        : this(httpClient, appDataDirectory, new FileSystem())
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CachingManifestService"/> class.
+    /// </summary>
+    /// <param name="httpClient">The HTTP client to use.</param>
+    /// <param name="appDataDirectory">The path to the app data directory to use.</param>
+    /// <param name="fileSystem">The file system implementation to use.</param>
+    public CachingManifestService(HttpClient httpClient, string appDataDirectory, IFileSystem fileSystem)
         : base(httpClient)
     {
-        CacheDirectory = Path.Combine(appDataDirectory, "ManifestCache");
+        FileSystem = fileSystem;
+        CacheDirectory = FileSystem.Path.Combine(appDataDirectory, "ManifestCache");
     }
 
     /// <inheritdoc />
     public override async Task<Stream> GetFileDataAsync(ManifestFile file, CancellationToken ct = default)
     {
-        if (!Directory.Exists(CacheDirectory))
-            Directory.CreateDirectory(CacheDirectory);
+        if (!FileSystem.Directory.Exists(CacheDirectory))
+            FileSystem.Directory.CreateDirectory(CacheDirectory);
 
-        string filePath = Path.Combine(CacheDirectory, file.FileName);
-        FileInfo fileInfo = new(filePath);
+        string filePath = FileSystem.Path.Combine(CacheDirectory, file.FileName);
+        IFileInfo fileInfo = FileSystem.FileInfo.FromFileName(filePath);
 
         if (fileInfo.Exists && fileInfo.LastWriteTimeUtc >= file.Timestamp)
-            return File.OpenRead(filePath);
+            return FileSystem.File.OpenRead(filePath);
 
         Stream dataStream = await base.GetFileDataAsync(file, ct).ConfigureAwait(false);
-        await using FileStream fs = File.OpenWrite(filePath);
+        await using Stream fs = FileSystem.File.OpenWrite(filePath);
 
         await dataStream.CopyToAsync(fs, ct).ConfigureAwait(false);
         dataStream.Seek(0, SeekOrigin.Begin);

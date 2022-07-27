ea0d7a1a86d447679d667097547ffce8540e7f5b
Carl Stephens <carlst99@outlook.co.nz>
2022-07-27 17:24:50 +1200

    Begin adding VehicleAttachment.cs collection

---
+++ Sanctuary.Census/CollectionBuilders/VehicleAttachmentCollectionBuilder.cs
@@ -0,0 +1,54 @@
+﻿using Sanctuary.Census.Abstractions.CollectionBuilders;
+using Sanctuary.Census.Abstractions.Database;
+using Sanctuary.Census.ClientData.Abstractions.Services;
+using Sanctuary.Census.ClientData.ClientDataModels;
+using Sanctuary.Census.Exceptions;
+using Sanctuary.Census.Models.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sanctuary.Census.CollectionBuilders;
+
+/// <summary>
+/// Builds the <see cref="Models.Collections.VehicleAttachment"/> collection.
+/// </summary>
+public class VehicleAttachmentCollectionBuilder : ICollectionBuilder
+{
+    private readonly IClientDataCacheService _clientDataCache;
+    private readonly ILocaleDataCacheService _localeDataCache;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VehicleAttachmentCollectionBuilder"/> class.
+    /// </summary>
+    /// <param name="clientDataCache">The client data cache.</param>
+    /// <param name="localeDataCache">The locale data cache.</param>
+    public VehicleAttachmentCollectionBuilder
+    (
+        IClientDataCacheService clientDataCache,
+        ILocaleDataCacheService localeDataCache
+    )
+    {
+        _clientDataCache = clientDataCache;
+        _localeDataCache = localeDataCache;
+    }
+
+    /// <inheritdoc />
+    public async Task BuildAsync
+    (
+        IMongoContext dbContext,
+        CancellationToken ct
+    )
+    {
+        if (_clientDataCache.ClientItemDefinitions.Count == 0)
+            throw new MissingCacheDataException(typeof(ClientItemDefinition));
+
+        if (_clientDataCache.ItemVehicles.Count == 0)
+            throw new MissingCacheDataException(typeof(ItemVehicle));
+
+        if (_clientDataCache.VehicleLoadoutSlotItemClasses.Count == 0)
+            throw new MissingCacheDataException(typeof(VehicleLoadoutSlotItemClass));
+
+        List<VehicleAttachment> builtAttachments = new();
+    }
+}
---
+++ Sanctuary.Census/Models/Collections/VehicleAttachment.cs
@@ -0,0 +1,25 @@
+﻿using Sanctuary.Census.Attributes;
+using Sanctuary.Census.Common.Objects.CommonModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sanctuary.Census.Models.Collections;
+
+/// <summary>
+/// Represents a vehicle attachment.
+/// </summary>
+/// <param name="ItemID">The ID of the attachment item.</param>
+/// <param name="VehicleLoadoutID">The ID of a vehicle loadout that the attachment can be used on.</param>
+/// <param name="VehicleID">The ID of a vehicle that the attachment can be used on.</param>
+/// <param name="FactionID">The ID of a faction that the attachment can be used on.</param>
+/// <param name="VehicleLoadoutSlotID">The vehicle loadout slot that the attachment can be used in.</param>
+/// <param name="Description">The locale string ID of the vehicle's name.</param>
+[Collection]
+public record VehicleAttachment
+(
+    [property: Key] uint ItemID,
+    [property: Key] uint VehicleLoadoutID,
+    uint VehicleID,
+    uint FactionID,
+    uint VehicleLoadoutSlotID,
+    LocaleString? Description
+);

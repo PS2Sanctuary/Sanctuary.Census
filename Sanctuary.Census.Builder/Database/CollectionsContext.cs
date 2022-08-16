922c6b53374b961566d264eb55f815921d11bbcd
Carl Stephens <carlst99@outlook.co.nz>
2022-08-16 12:50:48 +1200

    Move collection classmap registration to common services initialization

--- Sanctuary.Census.Builder/Database/CollectionsContext.cs
+++ Sanctuary.Census.Builder/Database/CollectionsContext.cs
@@ -1,12 +1,10 @@
 ﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using Sanctuary.Census.Builder.Abstractions.Database;
 using Sanctuary.Census.Builder.Abstractions.Services;
 using Sanctuary.Census.Common;
 using Sanctuary.Census.Common.Abstractions.Services;
 using Sanctuary.Census.Common.Objects.Collections;
-using Sanctuary.Census.Common.Objects.CommonModels;
 using Sanctuary.Census.Common.Objects.DiffModels;
 using Sanctuary.Census.Common.Services;
 using System;
@@ -541,44 +539,5 @@ public class CollectionsContext : ICollectionsContext
                 ct
             ).ConfigureAwait(false);
         }
-    }
-
-    static CollectionsContext()
-    {
-        BsonClassMap.RegisterClassMap<Currency>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Experience>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<FacilityInfo>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<FacilityLink>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Faction>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<FireGroup>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<FireGroupToFireMode>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<FireMode2>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<FireModeToProjectile>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Item>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<ItemCategory>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<ItemToWeapon>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Loadout>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<LoadoutSlot>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<MapRegion>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<OutfitWar>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<OutfitWarRegistration>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<OutfitWarRounds>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<PlayerStateGroup2>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Profile>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Projectile>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Vehicle>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<VehicleAttachment>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<VehicleLoadout>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<VehicleLoadoutSlot>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<VehicleSkillSet>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Weapon>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<WeaponAmmoSlot>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<WeaponToAttachment>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<WeaponToFireGroup>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<World>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<Common.Objects.Collections.Zone>(MongoContext.AutoClassMap);
-
-        BsonClassMap.RegisterClassMap<LocaleString>(MongoContext.AutoClassMap);
-        BsonClassMap.RegisterClassMap<NewCollection>(MongoContext.AutoClassMap);
     }
 }

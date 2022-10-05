5acd89f38b23b2e16e2183cfd6b6c7ba6c85f988
Carl Stephens <carlst99@outlook.co.nz>
2022-10-05 23:06:34 +1300

    Add to FireMode2.cs: BulletArcKickAngle, CloakAfterFireDelayMs, FanAngle, FireNeedsLock, SprintAfterFireDelayMs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,12 +2,21 @@
 
 Date format: DD/MM/YYYY
 
-## vNext
+## 16/09/2022
 
+**fire_mode_2**
+- Added the `bullet_arc_kick_angle` field (`decimal`), which is the relative pitch angle in degrees at which a bullet exits the barrel.
+- Added the `cloak_after_fire_delay_ms` field (`uint?`), which is the delay in milliseconds before the infiltrator's cloak ability is usable, after firing.
+  Worth noting that this field likely affects more than just cloaking, but we can't determine what, if so, at this stage.
+- Added the `fan_angle` field (`decimal?`), which is the angle in degrees at which pellets such as those on the Horizon, are separated by.
+- Added the `fire_needs_lock` field (bool), which indicates whether a lock on a target is required before the fire mode can be used.
+- Added the `sprint_after_fire_delay_ms` field (`uint?`), which is the delay in milliseconds before sprinting can occur, after firing.
+
+**Other**
 - Added the `item.code_factory_name` field.
 - Added the `loadout.code_name` field. This brings the `loadout` collection to full parity with DBG Census.
+- `weapon_ammo_slot.clip_size` is now typed as signed 16-bit integer, rather than unsigned.
 - Improved some collection descriptions.
-- Added a temporary favicon.
 
 ## 16/09/2022
 
--- Sanctuary.Census.Common/Objects/Collections/FireMode2.cs
+++ Sanctuary.Census.Common/Objects/Collections/FireMode2.cs
@@ -6,7 +6,92 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Sanctuary.Census.Common.Objects.Collections;
 
-#pragma warning disable CS1591
+/// <summary>
+/// Represents fire mode data - the firing characteristics of a weapon when in a certain state, e.g. hipfire.
+/// </summary>
+/// <param name="FireModeID">The ID of the fire mode.</param>
+/// <param name="FireModeTypeID">The type of the fire mode.</param>
+/// <param name="Description">A description of the fire mode.</param>
+/// <param name="AbilityID">The ID of the ability that the fire mode applies.</param>
+/// <param name="AmmoItemID"></param>
+/// <param name="AmmoSlot"></param>
+/// <param name="ArmorPenetration"></param>
+/// <param name="Automatic"></param>
+/// <param name="BulletArcKickAngle">The relative pitch angle in degrees at which a bullet exits the barrel.</param>
+/// <param name="GriefImmune">Indicates whether grief points can be accumulated when the fire mode is active.</param>
+/// <param name="LaserGuided">Indicates whether projectiles will follow the crosshair when the fire mode is active.</param>
+/// <param name="IronSights"></param>
+/// <param name="SprintFire">Indicates whether the fire mode is usable while sprinting.</param>
+/// <param name="SwayCanSteady">Indicates whether any scope sway applied by the fire mode can be steadied.</param>
+/// <param name="UseInWater">Indicates whether the fire mode is usable underwater.</param>
+/// <param name="CloakAfterFireDelayMs">The delay in milliseconds before the infiltrator's cloak ability is usable, after firing.</param>
+/// <param name="CofOverride"></param>
+/// <param name="CofPelletSpread"></param>
+/// <param name="CofRange"></param>
+/// <param name="CofRecoil"></param>
+/// <param name="CofScalar"></param>
+/// <param name="CofScalarMoving"></param>
+/// <param name="DamageHeadMultiplier"></param>
+/// <param name="DamageLegsMultiplier"></param>
+/// <param name="DamageIndirectEffectID"></param>
+/// <param name="FanAngle">The angle in degrees at which pellets such as those on the Horizon, are separated by.</param>
+/// <param name="FireAmmoPerShot"></param>
+/// <param name="FireAutoFireMs"></param>
+/// <param name="FireBurstCount"></param>
+/// <param name="FireChargeUpMs"></param>
+/// <param name="FireCooldownDurationMs"></param>
+/// <param name="FireDelayMs"></param>
+/// <param name="FireDurationMs"></param>
+/// <param name="FireDetectRange"></param>
+/// <param name="FireNeedsLock">Indicates whether a lock on a target is required before the fire mode can be used.</param>
+/// <param name="FirePelletsPerShot"></param>
+/// <param name="FireRefireMs"></param>
+/// <param name="HeatPerShot"></param>
+/// <param name="HeatRecoveryDelayMs"></param>
+/// <param name="HeatThreshold"></param>
+/// <param name="MaxDamage"></param>
+/// <param name="MaxDamageRange"></param>
+/// <param name="MaxDamageInd"></param>
+/// <param name="MaxDamageIndRadius"></param>
+/// <param name="MinDamage"></param>
+/// <param name="MinDamageRange"></param>
+/// <param name="MinDamageInd"></param>
+/// <param name="MinDamageIndRadius"></param>
+/// <param name="MoveModifier">The movement speed multiplier applied by the fire mode.</param>
+/// <param name="TurnModifier">The turn speed multiplier applied by the fire mode.</param>
+/// <param name="PlayerStateGroupID"></param>
+/// <param name="ProjectileSpeedOverride"></param>
+/// <param name="RecoilAngleMax"></param>
+/// <param name="RecoilAngleMin"></param>
+/// <param name="RecoilFirstShotModifier"></param>
+/// <param name="RecoilHorizontalMax"></param>
+/// <param name="RecoilHorizontalMaxIncrease"></param>
+/// <param name="RecoilHorizontalMin"></param>
+/// <param name="RecoilHorizontalMinIncrease"></param>
+/// <param name="RecoilHorizontalTolerance"></param>
+/// <param name="RecoilIncrease"></param>
+/// <param name="RecoilIncreaseCrouched"></param>
+/// <param name="RecoilMagnitudeMax"></param>
+/// <param name="RecoilMagnitudeMin"></param>
+/// <param name="RecoilMaxTotalMagnitude"></param>
+/// <param name="RecoilRecoveryAcceleration"></param>
+/// <param name="RecoilRecoveryDelayMs"></param>
+/// <param name="RecoilRecoveryRate"></param>
+/// <param name="RecoilShotsAtMinMagnitude"></param>
+/// <param name="ReloadAmmoFillMs"></param>
+/// <param name="ReloadBlockAuto"></param>
+/// <param name="ReloadChamberMs"></param>
+/// <param name="ReloadContinuous"></param>
+/// <param name="ReloadLoopStartMs"></param>
+/// <param name="ReloadLoopEndMs"></param>
+/// <param name="ReloadTimeMs"></param>
+/// <param name="ShieldBypassPct"></param>
+/// <param name="SprintAfterFireDelayMs">The delay in milliseconds before sprinting can occur, after firing.</param>
+/// <param name="SwayAmplitudeX"></param>
+/// <param name="SwayAmplitudeY"></param>
+/// <param name="SwayPeriodX"></param>
+/// <param name="SwayPeriodY"></param>
+/// <param name="ZoomDefault"></param>
 [Collection]
 [Description("Specific properties of a weapon's firing characteristics")]
 public record FireMode2
@@ -19,12 +104,14 @@ public record FireMode2
     byte AmmoSlot,
     bool ArmorPenetration,
     bool Automatic,
+    decimal BulletArcKickAngle,
     bool GriefImmune,
     bool LaserGuided,
     bool IronSights,
     bool SprintFire,
     bool SwayCanSteady,
     bool UseInWater,
+    uint? CloakAfterFireDelayMs,
     decimal CofOverride,
     decimal CofPelletSpread,
     decimal CofRange,
@@ -34,6 +121,7 @@ public record FireMode2
     decimal? DamageHeadMultiplier,
     decimal? DamageLegsMultiplier,
     uint? DamageIndirectEffectID,
+    decimal? FanAngle,
     byte FireAmmoPerShot,
     ushort FireAutoFireMs,
     byte FireBurstCount,
@@ -42,6 +130,7 @@ public record FireMode2
     ushort FireDelayMs,
     ushort FireDurationMs,
     ushort FireDetectRange,
+    bool FireNeedsLock,
     byte FirePelletsPerShot,
     ushort FireRefireMs,
     uint? HeatPerShot,
@@ -84,10 +173,10 @@ public record FireMode2
     ushort? ReloadLoopEndMs,
     ushort ReloadTimeMs,
     int ShieldBypassPct,
+    uint? SprintAfterFireDelayMs,
     decimal? SwayAmplitudeX,
     decimal? SwayAmplitudeY,
     decimal? SwayPeriodX,
     decimal? SwayPeriodY,
     decimal ZoomDefault
 ) : ISanctuaryCollection;
-#pragma warning restore CS1591

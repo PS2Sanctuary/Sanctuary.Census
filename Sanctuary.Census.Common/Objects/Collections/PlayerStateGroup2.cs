a2ef32e015c28e0656e3cc95becddd71e588c055
Carl Stephens <carlst99@outlook.co.nz>
2022-10-22 20:50:09 +1300

    Update XML-doc on PlayerStateGroup2.cs

--- Sanctuary.Census.Common/Objects/Collections/PlayerStateGroup2.cs
+++ Sanctuary.Census.Common/Objects/Collections/PlayerStateGroup2.cs
@@ -4,7 +4,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Sanctuary.Census.Common.Objects.Collections;
 
-#pragma warning disable CS1591
+/// <summary>
+/// Represents player movement state data.
+/// </summary>
+/// <param name="PlayerStateGroupId">The ID of the group that this state belongs to.</param>
+/// <param name="PlayerStateId">The ID of the state.</param>
+/// <param name="CanIronSight">Indicates whether the player can ADS while in this state.</param>
+/// <param name="CofGrowRate">
+/// The rate in degrees/sec at which the player's cone of fire increases to a given value if it is currently smaller.
+/// </param>
+/// <param name="CofMax">The maximum size, in degrees, of the player's cone of fire.</param>
+/// <param name="CofMin">The minimum size, in degrees, of the player's cone of fire.</param>
+/// <param name="CofRecoveryDelayMs">
+/// The delay in milliseconds before the player's cone of fire begins reducing to the <paramref name="CofMin"/>.
+/// Any growth in the COF will reset this delay.
+/// </param>
+/// <param name="CofRecoveryRate">
+/// The rate in degrees/sec at which player's cone of fire reduces to <paramref name="CofMin"/>.
+/// </param>
+/// <param name="CofShotsBeforePenalty"></param>
+/// <param name="CofRecoveryDelayThreshold"></param>
+/// <param name="CofTurnPenalty"></param>
 [Collection]
 public record PlayerStateGroup2
 (
@@ -20,4 +40,3 @@ public record PlayerStateGroup2
     uint CofRecoveryDelayThreshold,
     decimal CofTurnPenalty
 ) : ISanctuaryCollection;
-#pragma warning restore CS1591

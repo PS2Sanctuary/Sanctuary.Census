eb45c542b7b60c69b509be07355c3425a3323455
Carl <carlst99@outlook.co.nz>
2023-03-20 13:21:03 +1300

    Add Sanctuary.Census.RealtimeHub.csproj

---
+++ Sanctuary.Census.RealtimeHub/Program.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sanctuary.Census.Common.Extensions;
+using Sanctuary.Census.Common.Json;
+using Sanctuary.Census.Common.Objects;
+using Sanctuary.Census.RealtimeHub.Services;
+using Serilog;
+using Serilog.Events;
+using System.Text.Json.Serialization;
+
+namespace Sanctuary.Census.RealtimeHub;
+
+/// <summary>
+/// The entry class of the application.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// The entry function of the application.
+    /// </summary>
+    /// <param name="args">The command line arguments.</param>
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Host.UseSystemd();
+
+        SetupLogger(builder.Configuration, builder.Environment);
+        builder.Host.UseSerilog();
+
+        builder.Services.Configure<CommonOptions>(builder.Configuration.GetSection(nameof(CommonOptions)));
+
+        builder.Services.AddCommonServices(builder.Environment);
+
+        builder.Services.AddGrpc();
+
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseJsonNamingPolicy();
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+            });
+
+
+        builder.Services.AddHostedService<RealtimeCollectionPruneWorker>();
+
+        WebApplication app = builder.Build();
+
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        });
+
+        app.UseSerilogRequestLogging();
+        app.UseHttpsRedirection();
+        app.MapGrpcService<RealtimeIngressService>();
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    private static void SetupLogger(IConfiguration configuration, IHostEnvironment environment)
+    {
+        string? seqIngestionEndpoint = configuration["LoggingOptions:SeqIngestionEndpoint"];
+        string? seqApiKey = configuration["LoggingOptions:SeqApiKey"];
+
+        LoggerConfiguration loggerConfig = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Grpc", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Serilog.AspNetCore.RequestLoggingMiddleware", LogEventLevel.Warning)
+            .MinimumLevel.Override("System.Net.Http.HttpClient", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console
+            (
+                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}"
+            );
+
+        bool useSeq = environment.IsProduction()
+            && !string.IsNullOrEmpty(seqIngestionEndpoint)
+            && !string.IsNullOrEmpty(seqApiKey);
+        if (useSeq)
+        {
+            Serilog.Core.LoggingLevelSwitch levelSwitch = new();
+            loggerConfig.MinimumLevel.ControlledBy(levelSwitch)
+                .WriteTo.Seq(seqIngestionEndpoint!, apiKey: seqApiKey, controlLevelSwitch: levelSwitch);
+        }
+
+        Log.Logger = loggerConfig.CreateLogger();
+    }
+}
---
+++ Sanctuary.Census.RealtimeHub/Services/RealtimeIngressService.cs
@@ -0,0 +1,149 @@
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Sanctuary.Census.Common.Abstractions.Services;
+using Sanctuary.Census.Common.Objects;
+using Sanctuary.Census.Common.Objects.Collections;
+using Sanctuary.Census.Common.Objects.CommonModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sanctuary.Census.RealtimeHub.Services;
+
+/// <summary>
+/// A service that handles gRPC calls for updating realtime data.
+/// </summary>
+public class RealtimeIngressService : RealtimeIngress.RealtimeIngressBase
+{
+    private readonly ILogger<RealtimeIngressService> _logger;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RealtimeIngressService"/> class.
+    /// </summary>
+    /// <param name="logger">The logging interface to use.</param>
+    /// <param name="serviceScopeFactory">The service scope factory.</param>
+    public RealtimeIngressService
+    (
+        ILogger<RealtimeIngressService> logger,
+        IServiceScopeFactory serviceScopeFactory
+    )
+    {
+        _logger = logger;
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    /// <summary>
+    /// Processes map state updates.
+    /// </summary>
+    /// <param name="request">The update request.</param>
+    /// <param name="context">The context.</param>
+    /// <returns>The result of the update.</returns>
+    public override async Task<Ok> SubmitMapStateUpdate(MapStateUpdate request, ServerCallContext context)
+    {
+        _logger.LogDebug
+        (
+            "Received map state update for {ZoneId}:{ZoneInstance} on {WorldId} update with {RegionCount} regions",
+            request.ZoneId,
+            request.ZoneInstance,
+            request.WorldId,
+            request.Regions.Count
+        );
+
+        List<ReplaceOneModel<MapState>> replacementModels = new();
+        foreach (MapRegionState region in request.Regions)
+        {
+            bool isContested = region.ContestingFactionId is not (int)FactionDefinition.None
+                || region.RemainingCaptureTimeMs > 0
+                || region.RemainingCtfFlags < region.CtfFlags;
+
+            ValueEqualityDictionary<FactionDefinition, int> popUpperBounds = new();
+            ValueEqualityDictionary<FactionDefinition, byte> popPercentages = new();
+
+            foreach ((int faction, int value) in region.FactionPopulationUpperBound)
+                popUpperBounds.Add((FactionDefinition)faction, value);
+
+            foreach ((int faction, int value) in region.FactionPopulationPercentage)
+                popPercentages.Add((FactionDefinition)faction, (byte)value);
+
+            MapState builtState = new
+            (
+                request.WorldId,
+                (ushort)request.ZoneId,
+                (ushort)request.ZoneInstance,
+                request.Timestamp.Seconds,
+                region.MapRegionId,
+                (byte)region.OwningFactionId,
+                isContested,
+                (byte)region.ContestingFactionId,
+                region.CaptureTimeMs,
+                region.RemainingCaptureTimeMs,
+                region.CtfFlags,
+                region.RemainingCtfFlags,
+                popUpperBounds,
+                popPercentages
+            );
+
+            ReplaceOneModel<MapState> replacementModel = new
+            (
+                Builders<MapState>.Filter.Eq(x => x.WorldId, builtState.WorldId)
+                & Builders<MapState>.Filter.Eq(x => x.ZoneId, builtState.ZoneId)
+                & Builders<MapState>.Filter.Eq(x => x.ZoneInstance, builtState.ZoneInstance)
+                & Builders<MapState>.Filter.Eq(x => x.MapRegionId, builtState.MapRegionId),
+                builtState
+            ) { IsUpsert = true };
+            replacementModels.Add(replacementModel);
+        }
+
+        await using AsyncServiceScope scope = _serviceScopeFactory.CreateAsyncScope();
+        IMongoContext dbContext = scope.ServiceProvider.GetRequiredService<IMongoContext>();
+
+        IMongoCollection<MapState> mapColl = dbContext.GetCollection<MapState>();
+        await mapColl.BulkWriteAsync(replacementModels, null, context.CancellationToken);
+
+        return new Ok();
+    }
+
+    /// <summary>
+    /// Processes world population updates.
+    /// </summary>
+    /// <param name="request">The update request.</param>
+    /// <param name="context">The context.</param>
+    /// <returns>The result of the update.</returns>
+    public override async Task<Ok> SubmitWorldPopulationUpdate(WorldPopulationUpdate request, ServerCallContext context)
+    {
+        _logger.LogDebug("Received world population update for {WorldId}", request.WorldId);
+
+        int total = 0;
+        ValueEqualityDictionary<FactionDefinition, int> pops = new();
+
+        foreach ((int faction, int value) in request.FactionPopulations)
+        {
+            pops.Add((FactionDefinition)faction, value);
+            total += value;
+        }
+
+        WorldPopulation worldPop = new
+        (
+            request.WorldId,
+            request.Timestamp.Seconds,
+            total,
+            pops
+        );
+
+        await using AsyncServiceScope scope = _serviceScopeFactory.CreateAsyncScope();
+        IMongoContext dbContext = scope.ServiceProvider.GetRequiredService<IMongoContext>();
+
+        IMongoCollection<WorldPopulation> popColl = dbContext.GetCollection<WorldPopulation>();
+        await popColl.ReplaceOneAsync
+        (
+            Builders<WorldPopulation>.Filter.Eq(x => x.WorldId, worldPop.WorldId),
+            worldPop,
+            new ReplaceOptions { IsUpsert = true },
+            context.CancellationToken
+        );
+
+        return new Ok();
+    }
+}
